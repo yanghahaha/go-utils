a86091336fd930bd80a7ff61608a3ee8da291426
woo <woo@wooos-MacBook-Pro.local>
2017-07-14 16:36:33 +0800

    modify logger

--- logger.go
+++ logger.go
@@ -145,23 +145,3 @@ func GetLogger() *ELogger {
 	}
 	return _loggerInstance
 }
-
-//StrFormatTime 字符串格式化为当前时间
-//%Y %m %d %H %i %s : year month day hour minutes second
-func StrFormatTime(instr string) (outstr string) {
-	//2006-01-02 15:04:05
-	nowtime := time.Now()
-	year := nowtime.Format("2006")
-	month := nowtime.Format("01")
-	day := nowtime.Format("02")
-	hour := nowtime.Format("15")
-	minutes := nowtime.Format("04")
-	sencond := nowtime.Format("05")
-	outstr = strings.Replace(instr, "%Y", year, -1)
-	outstr = strings.Replace(outstr, "%m", month, -1)
-	outstr = strings.Replace(outstr, "%d", day, -1)
-	outstr = strings.Replace(outstr, "%H", hour, -1)
-	outstr = strings.Replace(outstr, "%i", minutes, -1)
-	outstr = strings.Replace(outstr, "%s", sencond, -1)
-	return
-}
